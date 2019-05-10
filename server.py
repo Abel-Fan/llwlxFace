4012e9b7e59c7bbf7df7311c2e9aa7abf601160c
Abel-Fan <842615663@qq.com>
2019-05-10 11:02:37 +0800

    add

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import urllib,ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 app = Flask(__name__)
 
@@ -21,6 +24,24 @@ def success():
 @app.route("/error")
 def error():
     return render_template("error.html")
+
+@app.route("/face2")
+def face2():
+    return render_template("face2.html")
+
+@app.route("/photo",methods=['POST'])   # 检测人脸并且存储人脸
+def photo():
+    data = urllib.parse.urlencode(request.form).encode("utf8") # 编码
+    con = urllib.request.urlopen("https://118.190.150.35:5000/api/photo",data=data).read()
+    print(con)
+    return con
+
+@app.route("/check",methods=['POST'])
+def check():
+    data = urllib.parse.urlencode(request.form).encode("utf8")  # 编码
+    con = urllib.request.urlopen("https://118.190.150.35:5000/api/check", data=data).read()
+    print(con)
+    return con
 
 if __name__ =="__main__":
     app.run(debug=True)
